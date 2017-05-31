cb1b5f557e8a912bf169020cedb3c67e6ad6070f
agustinkoszczej <agustinkoszczej@hotmail.com>
2017-05-31 01:07:22 -0300

    Some adds before sleep

--- src/test/java/ar/edu/utn/frba/dds/IndicadoresTest.java
+++ src/test/java/ar/edu/utn/frba/dds/IndicadoresTest.java
@@ -16,15 +16,36 @@ public class IndicadoresTest {
 
 	  @Before
 	  public void init() {
-		indicadorW = new Indicador("IndicadorW", "5");  
+		indicadorW = new Indicador("IndicadorW", "15");  
 		indicadorX = new Indicador("IndicadorX", "cuentaX");
-		indicadorX = new Indicador("IndicadorY", "cuentaX + 5");
-		indicadorX = new Indicador("IndicadorZ", "IndicadorW + 5 + cuentaX");
+		indicadorY = new Indicador("IndicadorY", "cuentaX + 5");
+		indicadorZ = new Indicador("IndicadorZ", "IndicadorW + 5 + cuentaX");
+		//cuentaX = 20
 	  }
 
 	@Test
 	public void indicadorConValorSolamente(){
 		ExpressionParser expresionW = new ExpressionParser(indicadorW.getExpresion());
-		Assert.assertEquals((Integer)5, expresionW.calculate());
+		//Assert.assertEquals((Integer)15, expresionW.calculate());
+	}
+	@Test
+	public void indicadorConCuentaSolamente(){
+		ExpressionParser expresionX = new ExpressionParser(indicadorX.getExpresion());
+		/*System.out.println(expresionX.getExpression());
+		System.out.println(expresionX.getStackTokens());
+		System.out.println(expresionX.getStackOperators());
+		System.out.println(expresionX.getVaVariableNames());
+		System.out.println(expresionX.calculate());*/
+		//Assert.assertEquals((Integer)20, expresionX.calculate());
+	}
+	@Test
+	public void indicadorConCuentaYValor(){
+		ExpressionParser expresionY = new ExpressionParser(indicadorY.getExpresion());
+		//Assert.assertEquals((Integer)25, expresionY.calculate());
+	}
+	@Test
+	public void indicadorConIndicadorValorYCuenta(){
+		ExpressionParser expresionZ = new ExpressionParser(indicadorZ.getExpresion());
+		//Assert.assertEquals((Integer)40, expresionZ.calculate());
 	}
 }
