86b5736884dd476bee71194b73eeee38582e585a
centenonicolas <centeno.nico@gmail.com>
2017-10-18 18:30:43 -0300

    muestra metodologias existentes con condiciones

--- src/main/java/ar/edu/utn/frba/dds/spark/controllers/MetodologiasController.java
+++ src/main/java/ar/edu/utn/frba/dds/spark/controllers/MetodologiasController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 
+import ar.edu.utn.frba.dds.metodologia.Condicion;
 import ar.edu.utn.frba.dds.metodologia.Metodologia;
 import ar.edu.utn.frba.dds.modelo.Empresa;
 import ar.edu.utn.frba.dds.modelo.Indicador;
@@ -18,16 +19,40 @@ import spark.Response;
 
 public class MetodologiasController implements WithGlobalEntityManager, TransactionalOps{
 	
+	private List<Condicion> condiciones;
+	private List<Metodologia> metodologias;
+	
 	
 	
 	public ModelAndView mostrarMetodologias(Request req, Response res){
 		
 		ProveedorAcceso proveedor = new ProveedorAcceso();
 		List<Metodologia> metodologias = proveedor.obtenerMetodologias();
-
-		Map<String, List<Metodologia>> model = new HashMap<>();
-		model.put("metodologias", metodologias);
-
+		
+		//Map<String, List<Metodologia>> model = new HashMap<>();
+		Map<String, MetodologiasController> model = new HashMap<>();
+		
+		MetodologiasController controllerMetodologia = new MetodologiasController();
+		controllerMetodologia.setMetodologias(metodologias);
+		
+		model.put("controllerMetodologias", controllerMetodologia);
+		
 		return new ModelAndView(model, "metodologias/show.hbs");	
+	}
+
+	public List<Metodologia> getMetodologias() {
+		return metodologias;
+	}
+
+	public void setMetodologias(List<Metodologia> metodologias) {
+		this.metodologias = metodologias;
+	}
+
+	public List<Condicion> getCondiciones() {
+		return condiciones;
+	}
+
+	public void setCondiciones(List<Condicion> condiciones) {
+		this.condiciones = condiciones;
 	}
 }
