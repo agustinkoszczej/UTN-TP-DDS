ab1153ca41b05254cdf29cc32782d0404b64f296
lucaspolonuer <polonuer@gmail.com>
2017-11-23 00:31:17 -0300

    Added maven plugins for heroku deployment

--- src/main/java/ar/edu/utn/frba/dds/spark/server/Server.java
+++ src/main/java/ar/edu/utn/frba/dds/spark/server/Server.java
@@ -2,15 +2,24 @@ package ar.edu.utn.frba.dds.spark.server;
 
 import ar.edu.utn.frba.dds.util.BatchCuentas;
 import spark.Spark;
+import spark.Spark.*;
 import spark.debug.DebugScreen;
 
 public class Server {
 	public static void main(String[] args) {
 		BatchCuentas.verificarCuentas();
 		
-		Spark.port(9000);
+		Spark.port(getHerokuAssignedPort());
 		DebugScreen.enableDebugScreen();
 		Router.configure();
 	}
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 
 }
